a88f7a6e37ceaa9280d0932134046c738e42f724
Toa Games <manage@toagames.com>
2023-08-07 00:39:28 -0400

    test push

--- src/hooks/useToDos.ts
+++ src/hooks/useToDos.ts
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ToDosContext } from "../providers/ToDosProvider";
 import { TO_DOS_ACTION_TYPE } from "../providers/ToDosProvider/types";
 
+// just a basic hook
 export const useToDos = () => {
   const { state, dispatch } = useContext(ToDosContext);
 
